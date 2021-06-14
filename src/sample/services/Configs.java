a2f12aaf256d19a3706346d07eaf7cf2910b4161
Mozart <80453455+iMozarti@users.noreply.github.com>
2021-06-14 13:49:46 +0300

    Update Configs.java

--- src/sample/services/Configs.java
+++ src/sample/services/Configs.java
@@ -2,8 +2,8 @@ package sample.services;
 
 public class Configs {
     protected static String dbHost="localhost";
-    protected String dbPort="3306";
-    protected String dbUser="root";
-    protected String dbPass="12345";
+    protected String dbPort="Port";
+    protected String dbUser="User_name";
+    protected String dbPass="Password";
     protected String dbName="avocadomessage";
 }
